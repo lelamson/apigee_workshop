5800615aa881e0ee6cd5734f371bb734bd7050fd
Lamson Le <ll217@u.washington.edu>
2015-07-22 15:47:57 -0700

    add getReviews fxn

--- api/controllers/restaurants.js
+++ api/controllers/restaurants.js
@@ -3,17 +3,27 @@
 var request = require('request');
 
 module.exports = {
-  getRestaurants: getRestaurants
+  getRestaurants: getRestaurants,
+  getReviews: getReviews
 };
 
 function getRestaurants (req, res) {
-  request('https://api.usergrid.com/lamson/sandbox/restaurants', function (error, response, body) {
-    if (error) {
-      res.send(error);
+  request('https://api.usergrid.com/lamson/sandbox/restaurants?limit=999', function (err, response, body) {
+    if (err) {
+      res.send(err);
     } else {
-      var results = {};
-      results.entities = JSON.parse(body).entities;
-      res.json(results);
+      res.send(body);
+    }
+  });
+}
+
+function getReviews (req, res) {
+  var restID = req.swagger.params.id.value;
+  request("http://api.usergrid.com/lamson/sandbox/reviews?limit=999&ql=restID=" + restID, function (err, resp, body) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(body);
     }
   });
 }
